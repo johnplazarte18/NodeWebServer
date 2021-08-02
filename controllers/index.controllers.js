ce93b36451d4a816544e4241cbf71560cc967fa1
HenryGP14 <henry.perez2017@uteq.edu.ec>
2021-08-02 17:21:57 -0500

    Incorporación de variable de entorno

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,25 +1,29 @@
+// Lectura de variable de entorno (.env)
+require("dotenv").config();
 //conexion a posgresql
-const {Pool}=require('pg');
-const pool=new Pool({
-    host:'localhost',
-    user:'postgres',
-    password:'123456',
-    database:'juegos_olimpicos',
-    port:'5432'
-})
+const { Pool } = require("pg");
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
 
-const getUsers= async (req,res)=>{
+const getUsers = async (req, res) => {
     //Tiene su tiempo-await y async asincrono
-    const respuesta= await pool.query('SELECT * FROM deportes');
+    const respuesta = await pool.query("SELECT * FROM deportes");
     res.status(200).json(respuesta.rows);
-}
+};
 
-const getUsersId = async (req,res)=>{
-    const respuesta= await pool.query('SELECT * FROM deportes WHERE id=$1',[req.params.id]);
+const getUsersId = async (req, res) => {
+    const respuesta = await pool.query("SELECT * FROM deportes WHERE id=$1", [
+        req.params.id,
+    ]);
     res.status(200).json(respuesta.rows);
-}
+};
 
-module.exports={
+module.exports = {
     getUsersId,
-    getUsers
-}
+    getUsers,
+};
