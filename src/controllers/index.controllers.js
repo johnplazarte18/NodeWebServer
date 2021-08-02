cd13d3ba4fc32adfb16e2c41b1d8e61f105c3b60
jhonp <john.plazarte2017@uteq.edu.ec>
2021-08-02 12:03:38 -0500

    Países a paises, problemas en la ruta

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,7 +1,11 @@
 //conexion a posgresql
 const {Pool}=require('pg');
 new Pool({
-    
+    host:'localhost',
+    user:'postgres',
+    password:'123456',
+    database:'juegos_olimpicos',
+    port:'5432'
 })
 
 const getUsers= (req,res)=>{
