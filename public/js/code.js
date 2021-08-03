fe39879f8bd76867946ac4c86f03fc0789fd0c01
CarlosAlmeida2000 <carlos.almeida2017@uteq.edu.ec>
2021-08-02 20:45:47 -0500

    Añadiendo otras peticiones de Ajax

--- public/js/code.js
+++ public/js/code.js
@@ -1,19 +1,44 @@
-var lblPaises = [];
-console.log("Ejecutar");
+const selectCountry = document.querySelector('#country');
+const selectMedallas = document.querySelector('#medal');
+
 $.ajax({
     type: "GET",
     contentType: "application/json; charset=utf-8",
-    url: "http://localhost:4000/paises/",
+    url: "http://localhost:3000/paises",
     dataType: "json",
     success: function (data) {
         data.forEach((element) => {
-            lblPaises.push(element.paises);
-
-            //mostrar paises
-            $("#lista-paises").append("");
+            option = document.createElement('option');
+            option.value = element.iso3
+            option.text = element.pais
+            selectCountry.appendChild(option)
         });
     },
     error: function (data) {
         alert("Ocurrió un error al intentar obtener los países");
     },
 });
+
+
+$.ajax({
+    type: "GET",
+    contentType: "application/json; charset=utf-8",
+    url: "http://localhost:3000/medallas",
+    dataType: "json",
+    success: function (data) {
+        data.forEach((element) => {
+            option = document.createElement('option');
+            option.value = element.medalla
+            option.text = element.medalla
+            selectMedallas.appendChild(option)
+        });
+    },
+    error: function (data) {
+        alert("Ocurrió un error al intentar obtener las medallas");
+    },
+});
+
+/*selectElement.addEventListener('change', (event) => {
+    const resultado = document.querySelector('.resultado');
+    resultado.textContent = `Te gusta el sabor ${event.target.value}`;
+});*/
