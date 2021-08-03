4470ba2f311287ad9003e3f60218d62ab7a7d031
HenryGP14 <henry.perez2017@uteq.edu.ec>
2021-08-02 22:11:13 -0500

    Fortend Terminado

--- README.md
+++ README.md
@@ -1,98 +1,118 @@
 # Node Web Service
+
 ## Introducción
+
 Este proyecto consiste en crear y consumir un web services para obtener datos de los deportistas latinoamericanos que han ganado medallas de oro, plata y bronce. Estos datos son filtrados por país, mostrando así los participantes y las disciplinas en las que ganaron medallas.
+
 ## Tecnologías y Recursos
+
 ### Frontend
-* HTML5
-* CSS3
-* JS
-* Ajax
+
+-   HTML5
+-   CSS3
+-   JS
+-   Ajax
+
 ### Backend
-* Node.js
-* PostgreSQL
+
+-   Node.js
+-   PostgreSQL
+
 ### Paquetes de Node.js
-* express
-* pg
+
+-   express
+-   pg
 
 ## Desarrollo del backend (web services)
 
-* Paso 1:
+-   Paso 1:
 
 Descarga e instala [Node.js](https://nodejs.org/es/) en su ultima versión.
 
-* Paso 2:
+-   Paso 2:
 
 Buscar un lugar donde crear el proyecto y abre la terminal de tu sistema operativo en la ruta que elegiste.
 
-* Paso 3:
+-   Paso 3:
 
 Crea una carpeta con el nombre del proyecto
-```
+
+```bash
 mkdir NodeWebServices
+```
+
+```bash
 cd NodeWebServices
 ```
 
-* Paso 4:
+-   Paso 4:
 
 Abre la carpeta creada con cualquier editor de código, para este caso con Visual Studio Code.
 
-* Paso 5:
+-   Paso 5:
 
 Crea un archivo javascript llamado **index.js**
 
-* Paso 6:
+-   Paso 6:
 
 Instala el paquete [express](https://www.npmjs.com/package/express) para poder implementar un servidor y manejar rutas de manera fácil.
+
 ```bash
 npm install express
 ```
 
-* Paso 7:
+-   Paso 7:
 
 Instala el paquete [pg](https://www.npmjs.com/package/pg) para poder realizar las conexiones a la base de datos de PostgreSQL
+
 ```bash
 npm install pg
 ```
 
-* Paso 8:
+-   Paso 8:
 
 Instala el paquete [cookie-parser](https://www.npmjs.com/package/cookie-parser) para poder codificar objetos cookie.
+
 ```bash
 npm install cookie-parser
 ```
 
-* Paso 9:
+-   Paso 9:
 
 Instala el paquete [http-errors](https://www.npmjs.com/package/http-errors) para poder crear errores http al paquete express.
+
 ```bash
 npm install http-errors
 ```
 
-* Paso 10:
+-   Paso 10:
 
 Instala el paquete [morgan](https://www.npmjs.com/package/morgan), un middleware del registrador de solicitudes HTTP para node.js
+
 ```bash
 npm install morgan
 ```
 
-* Paso 11:
+-   Paso 11:
 
 Instala el paquete [ejs](https://www.npmjs.com/package/ejs), que trae consigo plnatillas de javascript embebidas.
+
 ```bash
 npm install ejs
 ```
 
-* Paso 12:
+-   Paso 12:
 
 Abre el motor de PostgreSQL y crea la base de datos con el nombre de **juegos_olimpicos**
 
-* Paso 13:
+-   Paso 13:
 
 Diríjase a la carpeta data de este repositorio y encontrará varios archivos csv y un txt que detalla la creación de las tablas y la inserción de los datos. Ubique todos los archivos csv en la raíz de su equipo "C:/", y siga los pasos del archivo txt.
 
-* Paso 14:
+-   Paso 14:
 
 Abra la terminal en la raíz del proyecto y ejecute la aplicación con el siguiente comando:
+
 ```bash
 npm start
 ```
--- public/js/code.js
+++ public/js/code.js
@@ -1,17 +1,17 @@
-const selectCountry = document.querySelector('#country');
-const selectMedallas = document.querySelector('#medal');
+const selectCountry = document.querySelector("#country");
+const selectMedallas = document.querySelector("#medal");
 
 $.ajax({
     type: "GET",
     contentType: "application/json; charset=utf-8",
-    url: "http://localhost:3000/paises",
+    url: "http://localhost:4000/paises",
     dataType: "json",
     success: function (data) {
         data.forEach((element) => {
-            option = document.createElement('option');
-            option.value = element.iso3
-            option.text = element.pais
-            selectCountry.appendChild(option)
+            option = document.createElement("option");
+            option.value = element.iso3;
+            option.text = element.pais;
+            selectCountry.appendChild(option);
         });
     },
     error: function (data) {
@@ -19,18 +19,17 @@ $.ajax({
     },
 });
 
-
 $.ajax({
     type: "GET",
     contentType: "application/json; charset=utf-8",
-    url: "http://localhost:3000/medallas",
+    url: "http://localhost:4000/medallas",
     dataType: "json",
     success: function (data) {
         data.forEach((element) => {
-            option = document.createElement('option');
-            option.value = element.medalla
-            option.text = element.medalla
-            selectMedallas.appendChild(option)
+            option = document.createElement("option");
+            option.value = element.medalla;
+            option.text = element.medalla;
+            selectMedallas.appendChild(option);
         });
     },
     error: function (data) {
@@ -38,33 +37,41 @@ $.ajax({
     },
 });
 
-selectCountry.addEventListener('change', (event) => {
+selectCountry.addEventListener("change", (event) => {
+    document.getElementById("img_pais").src =
+        "/img/paises/" + selectCountry.value + ".png";
+
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
-        url: "http://localhost:3000/triunfo-pais/"+event.target.value,
+        url: "http://localhost:4000/triunfo-pais/" + event.target.value,
         dataType: "json",
         success: function (data) {
-            $('.card').empty()
+            $(".card").empty();
             data.forEach((element) => {
-                img = ''
-                if(element.sexo == 'Femenino'){
-                    img = '<img src="/img/people/Masculino.png" alt="">'
-                }else{
-                    img = '<img src="/img/people/Femenino.png" alt="">'
+                if (element.sexo == "Femenino") {
+                    img = '<img src="/img/people/Masculino.png" alt="">';
+                } else {
+                    img = '<img src="/img/people/Femenino.png" alt="">';
                 }
-                card = '<div class="card-people">'+
-                '<div class="img-people">'+
-                    img+
-                '</div>'+
-                '<div class="content">'+
-                  '<p><strong>Nombre:</strong>'+element.participante+'</p>'+
-                  '<p><strong>Deporte:</strong>'+element.deporte+'</p>'+
-                  '<p><strong>Competición:</strong>'+element.competicion+'</p>'+
-                '</div>'+
-              '</div>';
+                card =
+                    '<div class="card-people">' +
+                    '<div class="img-people">' +
+                    img +
+                    "</div>" +
+                    '<div class="content">' +
+                    "<p><strong>Nombre:</strong>" +
+                    element.participante +
+                    "</p>" +
+                    "<p><strong>Deporte:</strong>" +
+                    element.deporte +
+                    "</p>" +
+                    "<p><strong>Competición:</strong>" +
+                    element.competicion +
+                    "</p>" +
+                    "</div>" +
+                    "</div>";
                 $(".card").prepend(card);
-                //$("#img_pais").attr("src", element.ruta_foto);
             });
         },
         error: function (data) {
@@ -73,31 +80,40 @@ selectCountry.addEventListener('change', (event) => {
     });
 });
 
-selectMedallas.addEventListener('change', (event) => {
+selectMedallas.addEventListener("change", (event) => {
+    document.getElementById("img_medal").src =
+        "/img/medals/" + selectMedallas.value + ".png";
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
-        url: "http://localhost:3000/triunfo-medalla/"+event.target.value,
+        url: "http://localhost:4000/triunfo-medalla/" + event.target.value,
         dataType: "json",
         success: function (data) {
-            $('.card').empty()
+            $(".card").empty();
             data.forEach((element) => {
-                img = ''
-                if(element.sexo == 'Femenino'){
-                    img = '<img src="/img/people/Masculino.png" alt="">'
-                }else{
-                    img = '<img src="/img/people/Femenino.png" alt="">'
+                img = "";
+                if (element.sexo == "Femenino") {
+                    img = '<img src="/img/people/Masculino.png" alt="">';
+                } else {
+                    img = '<img src="/img/people/Femenino.png" alt="">';
                 }
-                card = '<div class="card-people">'+
-                '<div class="img-people">'+
-                    img+
-                '</div>'+
-                '<div class="content">'+
-                  '<p><strong>Nombre:</strong>'+element.participante+'</p>'+
-                  '<p><strong>Deporte:</strong>'+element.deporte+'</p>'+
-                  '<p><strong>Competición:</strong>'+element.competicion+'</p>'+
-                '</div>'+
-              '</div>';
+                card =
+                    '<div class="card-people">' +
+                    '<div class="img-people">' +
+                    img +
+                    "</div>" +
+                    '<div class="content">' +
+                    "<p><strong>Nombre:</strong>" +
+                    element.participante +
+                    "</p>" +
+                    "<p><strong>Deporte:</strong>" +
+                    element.deporte +
+                    "</p>" +
+                    "<p><strong>Competición:</strong>" +
+                    element.competicion +
+                    "</p>" +
+                    "</div>" +
+                    "</div>";
                 $(".card").prepend(card);
                 //$("#img_medal").attr("src", element.ruta_foto);
             });
